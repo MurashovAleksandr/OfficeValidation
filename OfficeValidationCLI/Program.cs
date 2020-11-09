886eaf6015d5bb17f21c74fb82d5ea33fb572127
Александр Мурашов <murashov_a@spb.ckb-rubin.local>
2020-11-09 13:21:01 +0300

    feat: Добавлена конфигурация Release

--- OfficeValidationCLI/Program.cs
+++ OfficeValidationCLI/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using CommandLine;
 using OfficeValidationLib.Classes;
@@ -8,33 +7,11 @@ using OfficeValidationLib.Enums;
 
 namespace OfficeValidationCLI
 {
-    class Options
-    {
-        [Option('c', 
-            "config", 
-            Required = true, 
-            HelpText = "Path to config json file. Content example: {\n\t\"Instances\":\n\t[\n\t\t{\n\t\t\t\"Name\":\"ContainsBookmarks2\",\n\t\t\t\"DisplayName\":\"Более 2 закладок\",\n\t\t\t\"Description\":\"Проверяется наличие закладок в документе (должно быть 2 или более)\",\n\t\t\t\"Assembly\":\"CommonOfficeValidationChecks.dll\",\n\t\t\t\"ClassName\":\"CommonOfficeValidationChecks.Checks.Bookmarks\",\n\t\t\t\"Parameters\":\n\t\t\t{\n\t\t\t\t\"BookmarksMinCount\":2\n\t\t\t}\n\t\t}\n\t]\n}")]
-        public string ConfigFile { get; set; }
-
-        [Option('d',
-            "documents", 
-            Required = true, 
-            HelpText = "Document files for check")]
-        public IEnumerable<string> DocumentFiles { get; set; }
-
-        [Option('s', 
-            "session_path", 
-            Required = false, 
-            HelpText = "Session directory path for save session log and checks result. Add '{0}' for set session id", 
-            Default = "sessions_{0}")]
-        public string SessionDirectoryPath { get; set; }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(RunOptions);
+            Parser.Default.ParseArguments<Options>(args).WithParsed(RunOptions);
         }
         static void RunOptions(Options opts)
         {
